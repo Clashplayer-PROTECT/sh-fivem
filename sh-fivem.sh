3a705a0026751bcf280270665f0ffca2aafe5f4c
Clashplayer <69047238+Clashplayer-PROTECT@users.noreply.github.com>
2021-07-19 20:24:42 +0200

    Update sh-fivem.sh

--- sh-fivem.sh
+++ sh-fivem.sh
@@ -76,6 +76,7 @@ printf "${CYAN} Démarrage de l'instalaltion de MARIADB pour serveur Five M !"
     apt update
     sudo add-apt-repository ppa:ondrej/php
     sudo apt-get update
+    sudo apt-get install phpmyadmin php-mbstring php-gettext
     sudo apt -y install php7.4
     apt install -y php7.4-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip} mariadb-client mariadb-server apache2 tar unzip git 
     php -v
@@ -93,6 +94,7 @@ if [[ "$reponse" == "o" ]]
 then 
 printf "${CYAN} Démarrage de l'instalaltion de phpMyAdmin pour serveur Five M !"
     apt install phpmyadmin
+    sudo service apache2 restart
     ln -s /usr/share/phpmyadmin/ /var/www/html/phpmyadmin
     echo "Lien du phpMyAdmin : http://$(hostname -I)/phpmyadmin/"
 fi
