7977017063b7c5bf31826cf59122d331d350e4ff
Clashplayer <69047238+Clashplayer-PROTECT@users.noreply.github.com>
2022-04-02 09:54:15 +0200

    OpenMENU

--- sh-beta.sh
+++ sh-beta.sh
@@ -15,3 +15,50 @@ COLOR3='\33[0;33m'
 COLOR4='\033[1;35m'
 NC='\033[0m'                                                #no color
 
+
+function OpenMENU() {
+        printf "${COLOR3} Bienvenue sur le contrôleur SH-FIVEM ! \\n"
+        printf "${COLOR2} Que voulez-vous faire ? \\n"
+        echo "   1) Création d'un serveur développement"
+        echo "   2) Mettez à jour votre artefact"
+        echo "   3) Installation d'une base FIVEM à partir de MEGA [BETA]"
+      	echo "   4) Création d'un nouvel utilisateur PhpMyAdmin"
+        echo "   5) BACKUP GOOGLE DRIVE"
+	    echo "   6) Quitter"
+        printf "${NC} \\n"
+
+	until [[ ${MENU_OPTION} =~ ^[1-4]$ ]]; do
+		read -rp "Sélectionnez une option [1-6]: " MENU_OPTION
+	done
+	case "${MENU_OPTION}" in
+	1)
+		installFIVEM
+		;;
+	2)
+		UpdateArtefact
+		;;
+
+   3)
+		installMEGA
+		;;
+   4)
+		newPHPMYADMIN
+		;;
+    5)
+		Backup-GoogleDRIVE
+		;;
+	6)
+		exit 0
+		;;
+	esac
+}
+
+
+
+
+if [[ -e /etc/sh-fivem/sh-install ]]; then
+	source /etc/sh-fivem/sh-install
+	OpenMENU
+else
+	installFIVEM
+fi
