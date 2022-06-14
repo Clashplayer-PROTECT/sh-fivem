fe332ed8b7657c2c8f3d68cd673917f8efc16c2c
Clashplayer <69047238+Clashplayer-PROTECT@users.noreply.github.com>
2022-06-14 23:01:30 +0200

    Update sh-beta.sh

--- sh-beta.sh
+++ sh-beta.sh
@@ -48,6 +48,7 @@ NC='\033[0m'                                                #no color
     printf "${COLOR1}©️  Copyright Tous droits réservés.©️ \\n"
 dist=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
     if [ "$dist" == "Ubuntu" ]; then
+        if [ "$dist" == "Debian" ]; then
         printf "${COLOR1}©️  Votre système d'exploitation est pris en charge par SH-FIVEM.©️ \\n"
     else
     printf "${COLOR2}💻 Malheureusement votre système d'exploitation n'est pas pris en charge par SH-FIVEM  💻\\n"
