76c157aa83de92c90a080464afddf33568b6fb70
Clashplayer <69047238+Clashplayer-PROTECT@users.noreply.github.com>
2024-03-14 17:57:13 +0100

    Update sh-fivem-new.sh

--- sh-fivem-new.sh
+++ sh-fivem-new.sh
@@ -106,7 +106,7 @@ sleep 2
 # Installation MARIADB
 echo
 printf "${CYAN} Démarrage de l'instalaltion de MariaDB pour serveur FiveM !"
-speed 4
+    apt  install lsb-release apt-transport-https ca-certificates -y
     sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
     echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
     apt update -y
